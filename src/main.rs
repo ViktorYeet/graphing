8b56444a21b2eddf92078208a0c54f5103137c76
viktoryeet <viktor@stefansbo.se>
2025-05-21 11:46:25 +0200

    testing custom plugin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use bevy::{prelude::*};
 
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, start);
+    }
+}
+
+fn start() {
+    println!("started")
+}
+        
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
+    .add_plugins(HelloPlugin)
     .run();
 }
