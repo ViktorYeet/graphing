f9915c1daed52db3a925faabd158455127fe4f35
ViktorYeet <viktor@stefansbo.se>
2025-05-21 22:34:39 +0200

    animated text

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bevy::{prelude::*};
+use bevy::{
+    prelude::*
+};
 
 pub struct HelloPlugin;
 
@@ -23,5 +25,45 @@ fn main() {
         ..default()
     }))
     .add_plugins(HelloPlugin)
+    .add_systems(Startup, setup)
+    .add_systems(Update, text_color_system)
     .run();
+}
+
+#[derive(Component)]
+struct AnimatedText;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Text::new("hello world!"),
+        TextFont {
+            font_size: 50.0,
+            ..default()
+        },
+        TextShadow::default(),
+        TextLayout::new_with_justify(JustifyText::Center),
+        BorderRadius::all(Val::Px(5.0)),
+        BorderColor(Color::linear_rgb(1.0, 0.0, 0.0)),
+        Node {
+            position_type: PositionType::Absolute,
+            align_self: AlignSelf::Center,
+            justify_self: JustifySelf::Center,
+            ..default()
+        },
+        AnimatedText,
+    ));
+}
+
+fn text_color_system(time: Res<Time>, mut query: Query<&mut TextColor, With<AnimatedText>>) {
+    for mut text_color in &mut query {
+        let seconds = time.elapsed_secs();
+
+        text_color.0 = Color::hsl(
+            ops::sin(seconds)*180.0 + 180.0, 
+            1.0,
+            0.5,
+        );
+    }
 }
