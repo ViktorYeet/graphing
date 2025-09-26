4a6cf881cda3d89bda1c775a92d3d603d94cff2c
ViktorYeet <viktor@stefansbo.se>
2025-09-26 22:39:19 +0200

    modified?

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     App::new()
     .add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
-            title: String::from("my window"),
+            title: String::from("Graphing?"),
             decorations: false,
             ..default()
         }),
